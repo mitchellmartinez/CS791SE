259976d33ea43aada6068e2a0c68cc7f8c98421f
mitchell martinez <mitchell.martinez@gmail.com>
2021-11-12 15:03:36 -0800

    Creating a wall in the scene. Adding new lights. Created metrics controller and attached to metrics container object. Set Exit button to only send metrics, not exit at this time. Reset later in button editor settings.

--- CS791-LabSpace/Assets/Scripts/ExitButton.cs
+++ CS791-LabSpace/Assets/Scripts/ExitButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public void exitButton()
     {
         Application.Quit();
---
+++ CS791-LabSpace/Assets/Scripts/MetricsController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+using Assets.Classes;
+
+public class MetricsController : MonoBehaviour
+{
+    // Create a new instance of Metrics 
+    public Metrics UserMetrics = new Metrics();
+
+    // Instance utilities for metric tracking
+    Stopwatch stopWatch = new Stopwatch();
+    int status = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopWatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update Metrics every frame 
+        UserMetrics.timeElapsed = stopWatch.ElapsedMilliseconds.ToString("mm\\:ss\\.ff");
+    }
+
+    public void SendMetrics()
+    {
+        System.Console.Write("User Metrics Sent");
+    }
+}
