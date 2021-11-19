3d220007bed53404619478f5754438579b2a3eef
mitchell martinez <mitchell.martinez@gmail.com>
2021-11-19 14:55:55 -0800

    Finished implementing light brightness slider in UI settings menu.

--- CS791-LabSpace/Assets/Scripts/LightsController.cs
+++ CS791-LabSpace/Assets/Scripts/LightsController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightsController : MonoBehaviour
 {
     public List<GameObject> lights;
+    public Slider lightSlider;
     public float lightIntensity = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        lightSlider.value = 0.5f;
+        lightSlider.maxValue = 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        lightIntensity = lightSlider.value;
         updateLightIntensity();
     }
 
@@ -25,6 +30,7 @@ public class LightsController : MonoBehaviour
             lamp.GetComponent<Light>().intensity = lightIntensity;
         }
     }
+
 
 }
 
